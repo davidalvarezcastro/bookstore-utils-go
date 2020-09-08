eccb92d30b8b45bcda071912be10c563593432f8
David Álvarez Castro <davidac0291@gmail.com>
2020-09-08 19:42:04 +0200

    moved errors into utils library

---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,52 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+// RestErr stores rest error info
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+// NewError return a new error
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+
+// NewBadRequestError return a status bad request
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+// NewNotFoundError return a not found request
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+// NewInternalServerError return an internal server error
+func NewInternalServerError(message string, err error) *RestErr {
+	result := &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+
+	if err != nil {
+		result.Causes = append(result.Causes, err.Error())
+	}
+
+	return result
+}
